f98c3cb065abc2edf7d2019902ac5072c8885554
Christine Sun <christine.sun@mesosphere.io>
2016-04-10 14:31:33 -0700

    we got math

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,16 @@
+#[link(name = "math")]
+extern "C" {
+    pub fn fmax(x:f64, y:f64) -> f64;
+}
+
 #[cfg(test)]
 mod test {
+    use super::*;
+    
     #[test]
     fn it_works() {
+        let m = unsafe { fmax(3.0, 4.0) };
+
+        assert!(m == 4.0);
     }
 }
